71e5de3db2cfaa49ed3b9fa0ea03561d6d306a01
Silviu Iordan <silviu.iordan@yahoo.com>
2020-10-31 18:36:03 +0200

    Adding validation and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
     def index
        @articles = Article.all        
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))  # witlisting the param that comes from the Internet
+        #render plain: @article.inspect  
+        if @article.save
+        #redirect_to article_path(@article)
+            flash[:notice] = "Article was created succesfully!"
+            redirect_to @article         # this will redirect tp the article route(path): /articles/:id
+        else
+            render 'new'
+        end
+    end
 end
