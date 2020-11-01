5c02a977be2040d1e2f5fa85ad24b3d24c4d8025
Silviu Iordan <silviu.iordan@yahoo.com>
2020-11-01 09:45:54 +0200

    Adding edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,11 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))  # witlisting the param that comes from the Internet
         #render plain: @article.inspect  
@@ -21,5 +26,15 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description)) 
+            flash[:notice] = "Article was updated succesfully."
+            redirect_to @article
+        else
+            render 'edit'
+        end    
     end
 end
