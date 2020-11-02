d16c6c6b8bf82e86291e5b5f5e67f79de208a06b
Silviu Iordan <silviu.iordan@yahoo.com>
2020-11-02 14:25:55 +0200

    DRY part 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
+
     def show
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def index
@@ -12,11 +15,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))  # witlisting the param that comes from the Internet
+        @article = Article.new(article_params)  # witlisting the param that comes from the Internet
         #render plain: @article.inspect  
         if @article.save
         #redirect_to article_path(@article)
@@ -28,8 +31,8 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description)) 
+        #@article = Article.find(params[:id])
+        if @article.update(article_params) 
             flash[:notice] = "Article was updated succesfully."
             redirect_to @article
         else
@@ -38,8 +41,19 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
+        #@article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path        
     end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
