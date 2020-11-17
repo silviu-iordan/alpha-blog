fedd868bbd46573c2758867d8ea819c0bd52172b
silviu-iordan <silviu.iordan@yahoo.com>
2020-11-17 12:49:12 +0200

    new users signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles      #now it will expose all the routes after the command "rails routes --expanded" [:show, :index, :new, :create, :edit, :update]
+  get 'signup', to: 'users#new' #when in the URL the "/signup" will be requested it will be routed to the "users" controller and it will use the action/method called "new"
+  resources :users, except: [:new]  #in this way we will get all of our routes for users but we already defined the new route as 'signup', the execept will ensure that we will get all the routes for users execpt for the 'new' that we already redifined
 end
 
