0203449460b487b26fb53392e4c96056906df4c7
silviu-iordan <silviu.iordan@yahoo.com>
2020-11-19 10:22:15 +0200

    Add show user profile and profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
 
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
+
 
   def edit
     @user = User.find(params[:id])
