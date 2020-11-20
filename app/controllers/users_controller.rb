4a4811bf7653c1de64894747c04feb68cccb3351
silviu-iordan <silviu.iordan@yahoo.com>
2020-11-20 18:21:47 +0200

    Cleanup layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was succesfully updated"
-      redirect_to articles_path
+      redirect_to user_path
     else
       render 'edit'
     end
