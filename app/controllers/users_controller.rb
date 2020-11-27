c5e0db935fcc171d11145a64c25dacdfe5f6ef77
silviu-iordan <silviu.iordan@yahoo.com>
2020-11-27 11:58:02 +0200

    Add delete user method and add admin column to users table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show,:edit,:update]
+    before_action :set_user, only: [:show,:edit,:update, :destroy] #this method will find the user before the actions show,edit,update,destroy are executed 
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy] #this will ensure that only the user looged in will be able to pergorm these actions
 
   def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -38,6 +38,14 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles succesfully deleted"
+    redirect_to articles_path
+  end
+
 
   private 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
-    has_many :articles
+    #"dependent" this will specify what to do t any depoendent of a User class, 
+    #if a user is deleted any articles of that user will be destroyed as well
+    has_many :articles, dependent: :destroy 
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum:3, maximum: 25 } 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
