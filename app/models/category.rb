1ad30dacec97dd27db50f7377a29c8316698216f
silviu-iordan <silviu.iordan@yahoo.com>
2020-12-03 15:41:38 +0200

    Add validations using unit tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
+    #this validation was added because of test "name should be present" from /test/models/category_test file
+    validates :name, presence: true, length: { minimum: 3, maximum: 25}
+
+    #this validation was added because of test "name should be unique" from /test/models/category_test file
+    validates_uniqueness_of :name
+
 
 end
